7a7a61fff32d1d621cb87ade9ef757256050c0d3
Abdulla-Kamal <elhassiabdo@gmail.com>
2023-12-31 13:27:32 +0200

    Push Admin Page First Time

---
+++ js/main.js
@@ -0,0 +1,81 @@
+//Check On Local Storage
+if (localStorage.getItem("--main-color")) {
+  document.documentElement.style.setProperty(
+    "--main-color",
+    localStorage.getItem("--main-color")
+  );
+  document.documentElement.style.setProperty(
+    "--secondry-color",
+    localStorage.getItem("--secondry-color")
+  );
+  document.documentElement.style.setProperty(
+    "--background-color",
+    localStorage.getItem("--background-color")
+  );
+  document.documentElement.style.setProperty(
+    "--white-color",
+    localStorage.getItem("--white-color")
+  );
+  document.documentElement.style.setProperty(
+    "--text-color",
+    localStorage.getItem("--text-color")
+  );
+  document.querySelectorAll(".settings .icon  i").forEach((i) => {
+    i.classList.remove("active");
+    if (i.dataset.maincolor === localStorage.getItem("--main-color")) {
+      i.classList.add("active");
+    }
+  });
+}
+
+// Start Header
+
+//Toggle Open Settings Class
+document
+  .querySelector(" .settings .gaer-icon")
+  .addEventListener("click", (_) => {
+    document.querySelector(".settings").classList.toggle("open");
+    document.querySelector(".settings .fa-gear").classList.toggle("fa-spin");
+  });
+
+//Switch Between Dark Mood And Sun Mood
+document.querySelectorAll(".settings .icon i").forEach((i) => {
+  i.addEventListener("click", function (e) {
+    e.target.parentElement.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    document.documentElement.style.setProperty(
+      "--main-color",
+      e.target.dataset.maincolor
+    );
+    document.documentElement.style.setProperty(
+      "--secondry-color",
+      e.target.dataset.secondrycolor
+    );
+    document.documentElement.style.setProperty(
+      "--background-color",
+      e.target.dataset.backgroundcolor
+    );
+    document.documentElement.style.setProperty(
+      "--white-color",
+      e.target.dataset.whitecolor
+    );
+    document.documentElement.style.setProperty(
+      "--text-color",
+      e.target.dataset.textcolor
+    );
+    localStorage.setItem("--main-color", e.target.dataset.maincolor);
+    localStorage.setItem("--secondry-color", e.target.dataset.secondrycolor);
+    localStorage.setItem(
+      "--background-color",
+      e.target.dataset.backgroundcolor
+    );
+    localStorage.setItem("--white-color", e.target.dataset.whitecolor);
+    localStorage.setItem("--text-color", e.target.dataset.textcolor);
+  });
+});
+// End Header
+
+
+
+
+

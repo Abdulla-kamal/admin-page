252dd54aa3c7c64663a2411fa2bdefc297864c1f
Abdulla-Kamal <elhassiabdo@gmail.com>
2024-01-03 18:32:47 +0200

    End Employees Section

--- js/main.js
+++ js/main.js
@@ -75,52 +75,94 @@ document.querySelectorAll(".settings .icon i").forEach((i) => {
 });
 // End Header
 
-
-// Start Main Page Height 
+// Start Main Page Height
 let headerHeight = document.querySelector("header").scrollHeight;
 let bodyHeight = document.body.scrollHeight;
-console.log(bodyHeight)
-document.querySelector(".main-page").style.height =`${(bodyHeight - headerHeight).toString()}px`;
+console.log(bodyHeight);
+document.querySelector(".main-page").style.height = `${(
+  bodyHeight - headerHeight
+).toString()}px`;
 // End Main Page Height
 
+// Start Add Employee Click
+const addEmplBtn = document.querySelector(
+  ".data[data-section=add-employees] .add-employees button"
+);
+const formAddEmp = document.querySelector(".form-add-employee");
+addEmplBtn.addEventListener("click", (_) => {
+  document.querySelector(".overlayForm").style.display = "block";
+  formAddEmp.style.display = "flex";
+});
+
+// Start Close Button
+formAddEmp.querySelector(".close").addEventListener("click", (_) => {
+  document.querySelector(".overlayForm").style.display = "none";
+  formAddEmp.style.display = "none";
+});
+// End Close Button
+
+// End Add Employee Click
+
+// Start Add Employee Click
+const editEmplForm = document.querySelector(
+  ".data[data-section=add-employees] .form-edit-employee"
+);
+document.querySelectorAll(".data table button.edit").forEach((em, i) => {
+  em.addEventListener("click", function (e) {
+    document.querySelector(".overlayForm").style.display = "block";
+    editEmplForm.style.display = "flex";
+  });
+});
+
+// Start Close Button
+editEmplForm.querySelector(".close").addEventListener("click", (_) => {
+  document.querySelector(".overlayForm").style.display = "none";
+  editEmplForm.style.display = "none";
+});
+// End Close Button
+
+// End Add Employee Click
 
 //Start File Click
 const employees = {
-  name: ["Abdulla Kamal", "Abdulrahman Almansoury", "Moftah Ali", "Ibrahim Jamal"],
-  number: [211, 546, 443, 708]
+  name: [
+    "Abdulla Kamal",
+    "Abdulrahman Almansoury",
+    "Moftah Ali",
+    "Ibrahim Jamal",
+  ],
+  number: [211, 546, 443, 708],
 };
-let filePromp = document.querySelector(".file-promp")
-document.querySelectorAll(".data table button.file").forEach((em, i)=> {
-  em.addEventListener("click", function(e) {
+let filePromp = document.querySelector(".file-promp");
+document.querySelectorAll(".data table button.file").forEach((em, i) => {
+  em.addEventListener("click", function (e) {
     document.querySelector(".overlay").style.display = "block";
     filePromp.querySelector(".name").innerHTML = employees.name[i];
     filePromp.querySelector(".number").innerHTML = employees.number[i];
     filePromp.style.display = "block";
-  })
-})
+  });
+});
 //Close The Promp
-filePromp.querySelector(".close").addEventListener("click",_=> {
+filePromp.querySelector(".close").addEventListener("click", (_) => {
   document.querySelector(".overlay").style.display = "none";
   filePromp.style.display = "none";
-})
+});
 
-// Start Click Add File 
-filePromp.querySelector(".add-file").addEventListener("click", _=> {
+// Start Click Add File
+filePromp.querySelector(".add-file").addEventListener("click", (_) => {
   document.querySelector(".overlayForm").style.display = "block";
   document.querySelector(".form-add-file").style.display = "flex";
-})
-// Close Th Add File 
-document.querySelector(".form-add-file").querySelector(".close").addEventListener("click",_=> {
-  document.querySelector(".overlayForm").style.display = "none";
-  document.querySelector(".form-add-file").style.display = "none";
-})
-// End Click Add File 
+});
+// Close Th Add File
+document
+  .querySelector(".form-add-file")
+  .querySelector(".close")
+  .addEventListener("click", (_) => {
+    document.querySelector(".overlayForm").style.display = "none";
+    document.querySelector(".form-add-file").style.display = "none";
+  });
+// End Click Add File
 //End File Click
-
-
 
 // Start Edit Click
 // End Edit Click
-
-
-
